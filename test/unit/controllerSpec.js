bedae340621d5a9f44ac4fde7d410447c1cbe0f8
Godfrey Chiu <ggwc82@gmail.com>
2016-03-10 17:08:30 +0000

    Unit test for GitUserCtrl completed

--- test/unit/controllerSpec.js
+++ test/unit/controllerSpec.js
@@ -5,15 +5,17 @@ describe('GitClone controllers', function() {
   describe('GitUserCtrl', function(){
     var scope, ctrl, $httpBackend;
 
-    beforeEach(module('gitcloneApp'));
-    beforeEach(inject(function(_$httpBackend_, $controller, $rootScope) {
-      $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('https://api.github.com/users').respond([{login: 'a1',
+    var fakeuser = [{login: 'a1',
       id: '25',
       avatar_url: 'http://test'},
       {login: 'a1',
       id: '25',
-      avatar_url: 'this is a magic line of words.'}]);
+      avatar_url: 'this is a magic line of words.'}]
+
+    beforeEach(module('gitcloneApp'));
+    beforeEach(inject(function(_$httpBackend_, $controller, $rootScope) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('https://api.github.com/users').respond(fakeuser);
 
       scope = $rootScope.$new();
       ctrl = $controller('GitUserCtrl', {$scope:scope});
@@ -22,10 +24,15 @@ describe('GitClone controllers', function() {
     it('should return list of "users"', function() {
       expect(scope.users).toBeUndefined();
       $httpBackend.flush();
-
       expect(scope.users.length).toBe(2);
-
     });
+
+    it('should return list of "users"', function() {
+      expect(scope.users).toBeUndefined();
+      $httpBackend.flush();
+      expect(scope.users).toEqual(fakeuser);
+    });
+
   });
 });
 
