fc1bed932de11a2b8674d65368926521a4af34fa
fareedpatel <fareed_patel@hotmail.com>
2016-03-10 18:13:57 +0000

    working progress

--- test/e2e-tests/scenarios.js
+++ test/e2e-tests/scenarios.js
@@ -4,5 +4,12 @@
 
 describe('my app', function() {
 
+	beforeEach(function(){
+		browser.get('/');
+	});
 
+	it('should have a list of 30 users', function(){
+		var list = element.all(by.css('.users list'));
+		expect(list.count()).toBe(30);
+	});
 });
