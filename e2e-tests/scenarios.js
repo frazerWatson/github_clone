9c6cffb0bfb2a1f4660958ed419603837416c348
Reiss Johnson <reissjohnson@me.com>
2016-03-10 12:08:21 +0000

    changed route to scenario.js file

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -6,7 +6,7 @@ describe('my app', function() {
 
 
   it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    browser.get('index.html');
+    browser.get('/');
     expect(browser.getLocationAbsUrl()).toMatch("/view1");
   });
 
